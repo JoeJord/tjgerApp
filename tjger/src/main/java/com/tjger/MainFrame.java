e9416f014bb1174bdd705471913975e54613ba05
josef <josef.jordan@gmx.net>
2022-06-29 21:48:41 +0200

    Removed v4 import.

--- tjger/src/main/java/com/tjger/MainFrame.java
+++ tjger/src/main/java/com/tjger/MainFrame.java
@@ -34,11 +34,12 @@ import android.app.FragmentTransaction;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
-import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
+
+import androidx.drawerlayout.widget.DrawerLayout;
 import at.hagru.hgbase.HGBaseWelcomeActivity;
 import at.hagru.hgbase.android.HGBaseAdvertisements;
 import at.hagru.hgbase.android.HGBaseResources;
