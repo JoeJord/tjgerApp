2e1da749a4d3a59cb841c71ca6443c96b875e324
josef <josef.jordan@gmx.net>
2024-03-18 17:44:15 +0100

    Java doc and formatting.

--- tjger/src/main/java/com/tjger/gui/gamepanel/configurablelayout/painter/IndexedElementSetPainter.java
+++ tjger/src/main/java/com/tjger/gui/gamepanel/configurablelayout/painter/IndexedElementSetPainter.java
@@ -5,7 +5,14 @@ import com.tjger.gui.completed.PartSet;
 import com.tjger.gui.completed.configurablelayout.layoutelement.IndexedLayoutElement;
 import com.tjger.gui.completed.configurablelayout.layoutelement.LayoutGameElementSet;
 
-public interface IndexedElementSetPainter <E extends LayoutGameElementSet, S extends PartSet, P extends Part> extends ElementSetPainter<E, S, P>{
+/**
+ * The interface for all painters for an element set of a configurable layout which may be indexed by the player index.
+ *
+ * @param <E> The type of the layout element.
+ * @param <S> The type of the game panel element set.
+ * @param <P> The type of the game panel element.
+ */
+public interface IndexedElementSetPainter<E extends LayoutGameElementSet, S extends PartSet, P extends Part> extends ElementSetPainter<E, S, P> {
     /**
      * Returns the index of the player for which the specified element should be painted.
      *
