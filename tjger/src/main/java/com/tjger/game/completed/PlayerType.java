1445b0b5d00406a9c62333eaf64f8341a268e5e4
Josef Jordan <josef.jordan@gmx.net>
2025-10-23 18:52:52 +0200

    Formatted and removed code smells.

--- tjger/src/main/java/com/tjger/game/completed/PlayerType.java
+++ tjger/src/main/java/com/tjger/game/completed/PlayerType.java
@@ -1,101 +1,35 @@
 package com.tjger.game.completed;
 
+import android.graphics.Bitmap;
+
+import androidx.annotation.NonNull;
+
 import com.tjger.game.internal.PlayerFactory;
 import com.tjger.lib.ArrayUtil;
 
-import android.graphics.Bitmap;
 import at.hagru.hgbase.lib.HGBaseItem;
 import at.hagru.hgbase.lib.HGBaseText;
 import at.hagru.hgbase.lib.HGBaseTools;
 
 /**
  * The player type.
- * 
+ *
  * @author hagru
  */
 public class PlayerType implements HGBaseItem {
 
-    final private String playerType;
-    final private Bitmap typeImage;
+    private final String playerTypeId;
+    private final Bitmap typeImage;
     /**
      * Flag if this player type is only available in the pro version but should be shown in the free version as teaser for the pro version.
      */
     private final boolean proTeaser;
 
-    public PlayerType(String playerType, Bitmap typeImage, boolean proTeaser) {
+    public PlayerType(String playerTypeId, Bitmap typeImage, boolean proTeaser) {
         super();
-        this.playerType = playerType;
+        this.playerTypeId = playerTypeId;
         this.typeImage = typeImage;
         this.proTeaser = proTeaser;
-    }
-    
-    /**
-     * @return The id of this player type.
-     */
-    @Override
-	public String getId() {
-        return playerType;
-    }
-    
-    /**
-     * @return The image for this player type, can be null.
-     */
-    public Bitmap getImage() {
-        return typeImage;
-    }
-
-    /* (non-Javadoc)
-     * @see java.lang.Object#toString()
-     */
-    @Override
-	public String toString() {
-        return HGBaseText.getText(getId());
-    }
-    
-    /**
-     * @return True, if it's a human player type.
-     */
-    public boolean isHuman() {
-        String[] hp = ArrayUtil.toPlayerTypeIds(PlayerFactory.getInstance().getHumanPlayerTypes()); 
-        return (HGBaseTools.getIndexOf(hp, getId())>=0);        
-    }
-    
-    /**
-     * @return True, if it's a network player type. 
-     */
-    public boolean isNetwork() {
-        String[] np = ArrayUtil.toPlayerTypeIds(PlayerFactory.getInstance().getNetworkPlayerTypes()); 
-        return (HGBaseTools.getIndexOf(np, getId())>=0);        
-    }
-    
-    
-    
-    /* (non-Javadoc)
-     * @see java.lang.Object#equals(java.lang.Object)
-     */
-    @Override
-	public boolean equals(Object o2) {
-        if (o2 instanceof PlayerType) {
-            return getId().equals(((PlayerType)o2).getId());            
-        }
-        return false;
-    }
-    
-    /* (non-Javadoc)
-     * @see java.lang.Object#hashCode()
-     */
-    @Override
-	public int hashCode() {
-        return getId().hashCode();
-    }
-
-    /**
-     * Returns {@code true} if this player type is only available in the pro version but should be shown in the free version as teaser for the pro version.
-     *
-     * @return {@code true} if this player type is only available in the pro version but should be shown in the free version as teaser for the pro version.
-     */
-    public boolean isProTeaser() {
-        return proTeaser;
     }
 
     /**
@@ -109,5 +43,61 @@ public class PlayerType implements HGBaseItem {
             return false;
         }
         return playerType.isProTeaser();
+    }
+
+    @Override
+    public String getId() {
+        return playerTypeId;
+    }
+
+    /**
+     * @return The image for this player type, can be null.
+     */
+    public Bitmap getImage() {
+        return typeImage;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return HGBaseText.getText(getId());
+    }
+
+    /**
+     * @return True, if it's a human player type.
+     */
+    public boolean isHuman() {
+        String[] hp = ArrayUtil.toPlayerTypeIds(PlayerFactory.getInstance().getHumanPlayerTypes());
+        return (HGBaseTools.getIndexOf(hp, getId()) >= 0);
+    }
+
+    /**
+     * @return True, if it's a network player type.
+     */
+    public boolean isNetwork() {
+        String[] np = ArrayUtil.toPlayerTypeIds(PlayerFactory.getInstance().getNetworkPlayerTypes());
+        return (HGBaseTools.getIndexOf(np, getId()) >= 0);
+    }
+
+    @Override
+    public boolean equals(Object o2) {
+        if (o2 instanceof PlayerType) {
+            return getId().equals(((PlayerType) o2).getId());
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return getId().hashCode();
+    }
+
+    /**
+     * Returns {@code true} if this player type is only available in the pro version but should be shown in the free version as teaser for the pro version.
+     *
+     * @return {@code true} if this player type is only available in the pro version but should be shown in the free version as teaser for the pro version.
+     */
+    public boolean isProTeaser() {
+        return proTeaser;
     }
 }
