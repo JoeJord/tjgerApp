495f0940f87d2a5fde1c594a5fd90169b98c276c
Josef Jordan <josef.jordan@gmx.net>
2025-10-23 18:57:19 +0200

    Formatted and removed code smells.

--- tjger/src/main/java/com/tjger/game/internal/PlayerFactory.java
+++ tjger/src/main/java/com/tjger/game/internal/PlayerFactory.java
@@ -1,9 +1,6 @@
 package com.tjger.game.internal;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import android.graphics.Bitmap;
 
 import com.tjger.game.GamePlayer;
 import com.tjger.game.HumanPlayer;
@@ -11,7 +8,11 @@ import com.tjger.game.NetworkPlayer;
 import com.tjger.game.completed.PlayerType;
 import com.tjger.lib.ArrayUtil;
 
-import android.graphics.Bitmap;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import at.hagru.hgbase.lib.internal.ClassFactory;
 
 /**
@@ -23,13 +24,13 @@ import at.hagru.hgbase.lib.internal.ClassFactory;
 public final class PlayerFactory {
 
     private static final PlayerFactory FACTORY = new PlayerFactory();
-    
-    final private Map<String,String> mapPlayerTypes = new HashMap<>();
-    final private List<PlayerType> supportTypes = new ArrayList<>(); // all player types
-    final private List<PlayerType> computerTypes = new ArrayList<>();
+
+    private final Map<String, String> mapPlayerTypes = new HashMap<>();
+    private final List<PlayerType> supportTypes = new ArrayList<>(); // all player types
+    private final List<PlayerType> computerTypes = new ArrayList<>();
+    private final List<PlayerType> networkTypes = new ArrayList<>();
+    private final List<PlayerType> humanTypes = new ArrayList<>();
     private String defaultComputerType;
-    final private List<PlayerType> networkTypes = new ArrayList<>();
-    final private List<PlayerType> humanTypes = new ArrayList<>();
 
     private PlayerFactory() {
         super();
@@ -44,9 +45,9 @@ public final class PlayerFactory {
 
     /**
      * @param playerType New player type to add.
-     * @param classPath The full name of the class that implements this player.
-     * @param typeImage The image of the player type.
-     * @param proTeaser Flag if the player type is only available in the pro version but should be shown in the free version as teaser for the pro version.
+     * @param classPath  The full name of the class that implements this player.
+     * @param typeImage  The image of the player type.
+     * @param proTeaser  Flag if the player type is only available in the pro version but should be shown in the free version as teaser for the pro version.
      * @return True, if player was added.
      */
     public boolean addPlayerType(String playerType, String classPath, Bitmap typeImage, boolean proTeaser) {
@@ -60,7 +61,7 @@ public final class PlayerFactory {
             } else if (player instanceof NetworkPlayer) {
                 networkTypes.add(ptNew);
             } else {
-            	computerTypes.add(ptNew);
+                computerTypes.add(ptNew);
             }
             return true;
         }
@@ -71,8 +72,8 @@ public final class PlayerFactory {
      * Creates a new player type, trying to get the default image.
      *
      * @param playerType The id of the new player type.
-     * @param typeImage The image of the player type.
-     * @param proTeaser Flag if the player type is only available in the pro version but should be shown in the free version as teaser for the pro version.
+     * @param typeImage  The image of the player type.
+     * @param proTeaser  Flag if the player type is only available in the pro version but should be shown in the free version as teaser for the pro version.
      * @return A new PlayerType object.
      */
     private PlayerType createPlayerType(String playerType, Bitmap typeImage, boolean proTeaser) {
@@ -87,7 +88,7 @@ public final class PlayerFactory {
      */
     public GamePlayer createPlayer(String playerType, String playerName, String pieceColor) {
         Object classPath = mapPlayerTypes.get(playerType);
-        if (classPath!=null) {
+        if (classPath != null) {
             // create the new player
             return createPlayer(classPath.toString(), playerType, playerName, pieceColor);
         }
@@ -95,16 +96,16 @@ public final class PlayerFactory {
     }
 
     /**
-     * @param classPath The full path for the class file.
+     * @param classPath  The full path for the class file.
      * @param playerType The id of a player type.
      * @param playerName The name of the new player.
      * @param pieceColor The piece color for this player.
      * @return A new player object from the given type.
      */
     @SuppressWarnings("rawtypes")
-	private GamePlayer createPlayer(String classPath, String playerType, String playerName, String pieceColor) {
-        Class[] classes = new Class[] { String.class, String.class, String.class };
-        Object[] params = new Object[] { playerType, playerName, pieceColor } ;
+    private GamePlayer createPlayer(String classPath, String playerType, String playerName, String pieceColor) {
+        Class[] classes = new Class[]{String.class, String.class, String.class};
+        Object[] params = new Object[]{playerType, playerName, pieceColor};
         return ClassFactory.createClass(classPath, GamePlayer.class, "player", classes, params);
     }
 
@@ -114,9 +115,9 @@ public final class PlayerFactory {
      */
     public PlayerType getPlayerType(String playerType) {
         PlayerType[] pt = getSupportedPlayerTypes();
-        for (int i=0; i<pt.length; i++) {
-            if (pt[i].getId().equals(playerType)) {
-                return pt[i];
+        for (PlayerType type : pt) {
+            if (type.getId().equals(playerType)) {
+                return type;
             }
         }
         return null;
@@ -128,7 +129,7 @@ public final class PlayerFactory {
      */
     public boolean isNetworkType(String playerType) {
         PlayerType pt = getPlayerType(playerType);
-        if (pt!=null) {
+        if (pt != null) {
             return pt.isNetwork();
         } else {
             return false;
@@ -141,19 +142,19 @@ public final class PlayerFactory {
      */
     public boolean isHumanType(String playerType) {
         PlayerType pt = getPlayerType(playerType);
-        if (pt!=null) {
+        if (pt != null) {
             return pt.isHuman();
         } else {
             return false;
         }
     }
-    
+
     /**
      * @param playerType the id of the player type
      * @return true if it is a computer type, i.e., neither human nor network
      */
     public boolean isComputerType(String playerType) {
-    	return !isHumanType(playerType) && !isNetworkType(playerType);
+        return !isHumanType(playerType) && !isNetworkType(playerType);
     }
 
     /**
@@ -185,20 +186,20 @@ public final class PlayerFactory {
     }
 
     /**
+     * Returns the default computer player type.
+     *
+     * @return The default computer player type.
+     */
+    public PlayerType getDefaultComputerType() {
+        return (isComputerType(defaultComputerType)) ? getPlayerType(defaultComputerType) : null;
+    }
+
+    /**
      * Sets the default computer player type.
      *
      * @param type The type to use as default for computer players.
      */
     public void setDefaultComputerType(String type) {
         defaultComputerType = type;
-    }
-
-    /**
-     * Returns the default computer player type.
-     *
-     * @return The default computer player type.
-     */
-    public PlayerType getDefaultComputerType() {
-	return (isComputerType(defaultComputerType)) ? getPlayerType(defaultComputerType): null; 
     }
 }
